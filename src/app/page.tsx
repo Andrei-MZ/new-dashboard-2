dc7ecf4112638fd6e52bb89b7377d5913a5095aa
andrei <andreimendes8@gmail.com>
2024-08-20 08:09:31 -0300

    cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl">Projeto dashboard</h1>
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <div>
+              <CardTitle>
+                Total Vendas
+              </CardTitle>
+            </div>
+          </CardHeader>
+        </Card>
+      </section>
     </main>
   );
 }
